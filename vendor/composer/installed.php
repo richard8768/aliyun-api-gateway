e1df6b07aeeb2f65be9c4206421c3929695d0a89
richard8768 <jobsen123@qq.com>
2022-03-26 12:26:57 +0800

    init aliyun-api-gateway

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'richard8768/aliyun-api-gateway',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'richard8768/aliyun-api-gateway' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
